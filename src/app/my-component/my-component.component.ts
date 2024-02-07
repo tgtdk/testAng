a19408d3780f6a67f7cc3f4bda219aaf6a114801
tgtdk <dhruvilharane188@gmail.com>
2024-02-08 00:18:30 +0530

    Commit

---
+++ src/app/my-component/my-component.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-component',
+  templateUrl: './my-component.component.html',
+  styleUrl: './my-component.component.scss'
+})
+export class MyComponentComponent {
+
+}
